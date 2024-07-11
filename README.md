c334eafc01b3faa2a64371d0d6666b24d45bf92d
Hugo EXTRAT <extrat.h@gmail.com>
2024-07-11 12:18:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# primer-sdk-rn
---
+++ android/src/main/java/com/primerioreactnative/utils/ErrorHelper.kt
@@ -0,0 +1,10 @@
+package com.primerioreactnative.utils
+
+import com.primerioreactnative.datamodels.ErrorTypeRN
+import com.primerioreactnative.datamodels.PrimerErrorRN
+
+infix fun ErrorTypeRN.errorTo(message: String) = PrimerErrorRN(
+  errorId = errorId,
+  errorCode = null,
+  description = message
+)
